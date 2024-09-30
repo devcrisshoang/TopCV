097662c767aaf33532fa4496ebd090d27700b234
anhduy2512033 <118426265+anhduy2512033@users.noreply.github.com>
2024-09-30 09:40:01 +0700

    Add Google and Maven build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.google.gms.google.services) apply false
 }
+buildscript { repositories {
+    google()
+    mavenCentral()
+}}
